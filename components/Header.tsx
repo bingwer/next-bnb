305c7a373303cfcf3a01b27a69c4ae1d518bc92b
Seunghyo Lee <nick.lee.5002@ecoflag.co.kr>
2021-08-06 18:35:37 +0900

    header 추가

---
+++ components/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Link from 'next/link';
+import styled from 'styled-components';
+import AirbnbLogoIcon from '../public/static/svg/logo/logo.svg';
+import AirbnbLogoText from '../public/static/svg/logo/logo_text.svg';
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 80px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.08) 0px 1px 12px;
+  z-index: 10;
+
+  .header-logo-wrapper {
+    display: flex;
+    align-items: center;
+    .header-logo {
+      margin-right: 6px;
+    }
+  }
+`;
+
+function Header() {
+  return (
+    <Container>
+      <Link href="/">
+        <a className="header-logo-wrapper">
+          <AirbnbLogoIcon className="header-logo" />
+          <AirbnbLogoText />
+        </a>
+      </Link>
+    </Container>
+  );
+}
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import { AppProps } from 'next/app';
 import React from 'react';
 import GlobalStyle from '../styles/GlobalStyle';
 
+import Header from '../components/Header';
+
 function app({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </>
   );
