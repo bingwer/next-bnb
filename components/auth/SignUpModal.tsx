ee1c01bde0da386915b317ab11a77291e59c0e46
Seunghyo Lee <nick.lee.5002@ecoflag.co.kr>
2021-08-09 15:36:12 +0900

    회원가입 모달창 추가

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,15 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Input from '../common/Input';
+import palette from '../../styles/palette';
+
+import CloseXIcon from '../../public/static/svg/modal/modal_close_x_icon.svg';
+import MailIcon from '../../public/static/svg/auth/mail.svg';
+import PersonIcon from '../../public/static/svg/auth/person.svg';
+import OpenedEyeIcon from '../../public/static/svg/auth/opened_eye.svg';
+import ClosedEyeIcon from '../../public/static/svg/auth/closed_eye.svg';
 
 const Container = styled.div`
   width: 568px;
-  height: 614px;
+  padding: 32px;
   background-color: white;
   z-index: 11;
+
+  .modal-close-x-icon {
+    cursor: pointer;
+    display: block;
+    margin: 0 0 40px auto;
+  }
+
+  .input-wrapper {
+    position: relative;
+    margin-bottom: 16px;
+  }
+
+  .sign-up-password-input-wrapper {
+    svg {
+      cursor: pointer;
+    }
+  }
 `;
 
 function SignUpModal() {
-  return <Container>Sign Up</Container>;
+  const [email, setEamil] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [password, setPassword] = useState('');
+  const [hidePassword, setHidePassword] = useState(true);
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEamil(e.target.value);
+  };
+
+  const onChangeLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
+  };
+
+  const onChangeFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const toggleHidePassword = () => {
+    setHidePassword(!hidePassword);
+  };
+
+  return (
+    <Container>
+      <CloseXIcon className="modal-close-x-icon" />
+      <div className="input-wrapper">
+        <Input
+          placeholder="이메일 주소"
+          type="email"
+          value={email}
+          onChange={onChangeEmail}
+          icon={<MailIcon />}
+        />
+      </div>
+      <div className="input-wrapper">
+        <Input
+          placeholder="이름(예: 길동)"
+          value={lastName}
+          onChange={onChangeLastName}
+          icon={<PersonIcon />}
+        />
+      </div>
+      <div className="input-wrapper">
+        <Input
+          placeholder="성(예: 홍)"
+          value={firstName}
+          onChange={onChangeFirstName}
+          icon={<PersonIcon />}
+        />
+      </div>
+      <div className="input-wrapper sign-up-password-input-wrapper">
+        <Input
+          placeholder="비밀번호"
+          type={hidePassword ? 'password' : 'text'}
+          value={password}
+          onChange={onChangePassword}
+          icon={
+            hidePassword ? (
+              <ClosedEyeIcon onClick={toggleHidePassword} />
+            ) : (
+              <OpenedEyeIcon onClick={toggleHidePassword} />
+            )
+          }
+        />
+      </div>
+    </Container>
+  );
 }
 
 export default SignUpModal;
