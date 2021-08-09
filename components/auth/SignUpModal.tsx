8677f3eb72694334c4db682ce279de6aa7f31d77
Seunghyo Lee <nick.lee.5002@ecoflag.co.kr>
2021-08-09 16:56:16 +0900

    회원가입 모달창 셀렉터, 버튼 추가

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -8,6 +8,9 @@ import MailIcon from '../../public/static/svg/auth/mail.svg';
 import PersonIcon from '../../public/static/svg/auth/person.svg';
 import OpenedEyeIcon from '../../public/static/svg/auth/opened_eye.svg';
 import ClosedEyeIcon from '../../public/static/svg/auth/closed_eye.svg';
+import Selector from '../common/Selector';
+import { dayList, monthList, yearList } from '../../lib/staticData';
+import Button from '../common/Button';
 
 const Container = styled.div`
   width: 568px;
@@ -31,14 +34,52 @@ const Container = styled.div`
       cursor: pointer;
     }
   }
+
+  .sign-up-birthdat-label {
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+
+  .sign-up-modal-birthday-info {
+    margin-bottom: 16px;
+    color: ${palette.charcoal};
+  }
+
+  .sign-up-modal-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+    .sign-up-modal-birthday-month-selector {
+      margin-right: 16px;
+      flex-grow: 1;
+    }
+    .sign-up-modal-birthday-day-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-modal-birthday-year-selector {
+      width: 33.3333%;
+    }
+  }
+
+  .sign-up-modal-submit-button-wrapper {
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${palette.gray_eb};
+  }
 `;
 
 function SignUpModal() {
-  const [email, setEamil] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [password, setPassword] = useState('');
-  const [hidePassword, setHidePassword] = useState(true);
+  const [email, setEamil] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [firstName, setFirstName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [hidePassword, setHidePassword] = useState<boolean>(true);
+
+  const [birthYear, setBirthYear] = useState<string | undefined>();
+  const [birthMonth, setBirthMonth] = useState<string | undefined>();
+  const [birthDay, setBirthDay] = useState<string | undefined>();
 
   const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEamil(e.target.value);
@@ -58,6 +99,17 @@ function SignUpModal() {
 
   const toggleHidePassword = () => {
     setHidePassword(!hidePassword);
+  };
+
+  const onChangeBirthYear = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthYear(e.target.value);
+  };
+
+  const onChangeBirthMonth = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthMonth(e.target.value);
+  };
+  const onChangeBirthDay = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthDay(e.target.value);
   };
 
   return (
@@ -102,6 +154,43 @@ function SignUpModal() {
             )
           }
         />
+      </div>
+      <p className="sign-up-birthdat-label">생일</p>
+      <p className="sign-up-modal-birthday-info">
+        만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른
+        에어비앤비 이용자에게 공개되지 않습니다.
+      </p>
+      <div className="sign-up-modal-birthday-selectors">
+        <div className="sign-up-modal-birthday-month-selector">
+          <Selector
+            options={monthList}
+            disabledOptions={['월']}
+            defaultValue="월"
+            value={birthMonth}
+            onChange={onChangeBirthMonth}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-day-selector">
+          <Selector
+            options={dayList}
+            disabledOptions={['일']}
+            defaultValue="일"
+            value={birthDay}
+            onChange={onChangeBirthDay}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-year-selector">
+          <Selector
+            options={yearList}
+            disabledOptions={['년']}
+            defaultValue="년"
+            value={birthYear}
+            onChange={onChangeBirthYear}
+          />
+        </div>
+      </div>
+      <div className="sign-up-modal-submit-button-wrapper">
+        <Button type="submit">가입하기</Button>
       </div>
     </Container>
   );
