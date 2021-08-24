2126bf3a29f9cc75f712c3fb5b750bc71d929c28
Seunghyo Lee <nick.lee.5002@ecoflag.co.kr>
2021-08-24 14:06:58 +0900

    숙소 유형 라디오 공통 컴포넌트 추가

--- components/register/RegisterRoomBuilding.tsx
+++ components/register/RegisterRoomBuilding.tsx
@@ -5,6 +5,7 @@ import { largeBuildingTypeList } from '../../lib/staticData';
 import { useSelector } from '../../store';
 import { registerRoomActions } from '../../store/registerRoom';
 import palette from '../../styles/palette';
+import RadioGroup from '../common/RadioGroup';
 import Selector from '../common/Selector';
 
 const Container = styled.div`
@@ -24,9 +25,35 @@ const Container = styled.div`
     width: 320px;
     margin-bottom: 32px;
   }
+
+  .register-room-room-type-radio {
+    max-width: 485px;
+    margin-bottom: 50px;
+  }
 `;
 
 const disabledlargeBuildingTypeOptions = ['하나를 선택해 주세요.'];
+
+const roomTypeRadioOptions = [
+  {
+    label: '집 전체',
+    value: 'entire',
+    description:
+      '게스트가 숙소 전체를 다른 사람과 공유하지 않고 단독으로 이용합니다. 일반적으로 침실, 욕실, 부엌이 포함됩니다.',
+  },
+  {
+    label: '개인실',
+    value: 'private',
+    description:
+      '게스트에게 개인 침실이 제공됩니다. 침실 이외의 공간은 공용일 수 있습니다.',
+  },
+  {
+    label: '다인실',
+    value: 'public',
+    description:
+      '게스트는 개인 공간 없이, 다른 사람과 함께 쓰는 침실이나 공용공간에서 숙박합니다.',
+  },
+];
 
 function RegisterRoomBuilding() {
   const dispatch = useDispatch();
@@ -34,6 +61,7 @@ function RegisterRoomBuilding() {
     state => state.registerRoom.largeBuildingType,
   );
   const buildingType = useSelector(state => state.registerRoom.buildingType);
+  const roomType = useSelector(state => state.registerRoom.roomType);
 
   const detailBuildingOptions = useMemo(() => {
     switch (largeBuildingType) {
@@ -94,6 +122,12 @@ function RegisterRoomBuilding() {
     dispatch(registerRoomActions.setBuildingType(e.target.value));
   };
 
+  const onChangeRoomType = (value: any) => {
+    dispatch(
+      registerRoomActions.setRoomType(value as 'entire' | 'private' | 'public'),
+    );
+  };
+
   return (
     <Container>
       <h2>등록할 숙소 종류는 무엇인가요?</h2>
@@ -119,6 +153,16 @@ function RegisterRoomBuilding() {
           onChange={onChangeBuildingType}
         />
       </div>
+      {buildingType && (
+        <div className="register-room-room-type-radio">
+          <RadioGroup
+            label="게스트가 묵게 될 숙소 유형을 골라주세요."
+            value={roomType}
+            options={roomTypeRadioOptions}
+            onChange={onChangeRoomType}
+          />
+        </div>
+      )}
     </Container>
   );
 }
