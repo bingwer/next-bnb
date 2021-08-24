44995f309b6f16f9134f62cc8f678fc060a782a1
Seunghyo Lee <nick.lee.5002@ecoflag.co.kr>
2021-08-24 15:37:46 +0900

    침실 페이지 및 관련 리덕스 스토어 추가

---
+++ pages/room/register/bedrooms.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterRoomBedRooms from '../../../components/register/RegisterRoomBedRooms';
+
+function bedrooms() {
+  return <RegisterRoomBedRooms />;
+}
+
+export default bedrooms;
